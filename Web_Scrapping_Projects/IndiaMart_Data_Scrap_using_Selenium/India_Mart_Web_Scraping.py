b3f9e8bd7277c7e031605348468c9ca1654722ff
Rudraneel Chakraborty <69258299+RudraneelChakraborty@users.noreply.github.com>
2023-08-07 12:55:28 +0530

    Update India_Mart_Web_Scraping.py

--- Web_Scrapping_Projects/IndiaMart_Data_Scrap_using_Selenium/India_Mart_Web_Scraping.py
+++ Web_Scrapping_Projects/IndiaMart_Data_Scrap_using_Selenium/India_Mart_Web_Scraping.py
@@ -7,10 +7,12 @@ import os
 
 driver = webdriver.Chrome()
 
-#driver.get('https://dir.indiamart.com/bengaluru/laminated-sheet.html')
+#driver.get('https://dir.indiamart.com/search.mp?ss=Saree&prdsrc=1&cq=Kolkata&cityid=70772&stype=attr=1|attrS&res=RC5')
 driver.get(input('Give the URL to Scrap:\n '))
 
 driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+
+WebDriverWait(driver= driver, timeout=2)
 
 storeDetails = driver.find_elements(By.CLASS_NAME,'clg')
 names = driver.find_elements(By.CLASS_NAME,'lcname')
@@ -20,16 +22,32 @@ namelist = []
 addresslist = []
 numberslist = []
 
-# take all details in append into the list
-for i in range(len(names)):
-    namelist.append(names[i].text)
-    addresslist.append(storeDetails[i].text)
-    wait = WebDriverWait(driver, 10)
-    contact = contact_list[i].get_attribute('innerHTML')
-    numberslist.append(contact)
+# take all details in append into list, But if storedetails length is not 0::
+if len(storeDetails) != 0:
+    for i in range(len(names)):
     
+        namelist.append(names[i].text)
+        addresslist.append(storeDetails[i].text)
+        wait = WebDriverWait(driver, 10)
+        contact = contact_list[i].get_attribute('innerHTML')
+        numberslist.append(contact)
 
-#Create a Dictionary out of it
+# This is for another scenario, where after searching indiamart that keep classes on different way::::
+if len(storeDetails) == 0:
+    dirty_addresslist = []
+    for i in range(len(names)):
+        storeDetails = driver.find_element(By.XPATH,f"//*[@id='citytt{i+1}']/span/p")
+        dirty_addresslist.append(storeDetails.get_attribute('innerHTML'))
+    addresslist = [address.replace('\n',' ') for address in dirty_addresslist]
+    
+    for i in range(len(names)):
+        namelist.append(names[i].text)
+        wait = WebDriverWait(driver, 10)
+        contact = contact_list[i].get_attribute('innerHTML')
+        numberslist.append(contact)
+
+    
+#Create Dictionary out of it
 data = {
         'Company/Store Name': namelist,
         'Address':addresslist,
@@ -38,7 +56,7 @@ data = {
 
 df = pd.DataFrame(data)
 
-#Check the filename if the file already exists or not, If exist then that keep increasing the number or occurrence
+#Check the filename if the file is already exist or not, If exist then that keep increament the number or occurance
 file_name = f"India_Mart_Web_Scrapping_{dt.date.today()}.csv"
 if os.path.isfile(file_name):
     expand = 1
@@ -52,6 +70,8 @@ if os.path.isfile(file_name):
             break
 
 #Load the file on same directory
-df.to_csv(file_name,header=False)
+df.to_csv(file_name,header=True)
 print(f'Created Filename: {file_name}')
 driver.close()
+
+
